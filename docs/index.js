7027a51511d9697eafa0cf70a324b8ec99567701
Daniel Brockman <daniel@brockman.se>
2017-06-14 13:56:20 +0300

    UI wip

--- docs/index.js
+++ docs/index.js
@@ -4,13 +4,32 @@ var eos_sale, eos_token
 
 var WAD = 1000000000000000000
 
+var hopefully = $ => (error, result) => {
+  if (error) {
+    lament(error)
+  } else {
+    $(result)
+  }
+}
+
+function lament(error) {
+  if (error) {
+    append("app", `
+      <div class="error pane">
+        <h3>${error.message}</h3>
+        <pre>${error.stack}</pre>
+      </div>
+    `)
+  }
+}
+
 function updateBalance() {
   eos_token.balanceOf(web3.eth.accounts[0], function(error, balance) {
     render("eos-balance", `${balance.div(WAD)} EOS`)
   })
 }
 
-onload = () => {
+onload = () => setTimeout(() => {
   if (!window.web3) {
     document.body.innerHTML = `
       <h1>No web3 provider detected</h1>
@@ -20,98 +39,164 @@ onload = () => {
     eos_sale  = web3.eth.contract(eos_sale_abi).at(eos_sale_address_kovan)
     eos_token = web3.eth.contract(eos_token_abi).at(eos_token_address_kovan)
 
-    eos_sale.numberOfDays((error, days) => {
+    async.parallel(Object.assign({
+      days: $ => eos_sale.numberOfDays($),
+    }, web3.eth.accounts[0] ? {
+      eth_balance: $ => web3.eth.getBalance(web3.eth.accounts[0], $),
+    } : {}), hopefully(({ eth_balance, days }) => {
       async.map(iota(Number(days)), (i, $) => {
-        var day = { id: Number(i) }
-        eos_sale.createOnDay(day.id, (error, forSale) => {
-          eos_sale.dailyTotals(day.id, (error, contributions) => {
+        var day = { id: i }
+        eos_sale.createOnDay(day.id, hopefully(forSale => {
+          eos_sale.dailyTotals(day.id, hopefully(contributions => {
             day.name = day.id + 1
             day.tokensForSale = forSale.div(WAD)
             day.contributions = contributions.div(WAD)
             day.price = contributions.div(forSale)
             $(null, day)
-          })
-        })
-      }, (error, days) => {
-        render("app", `
-          <h2>EOS Token Sale</h2>
+          }))
+        }))
+      }, hopefully(days => render("app", `
+        <h1>EOS Token Sale</h1>
 
-          <p>
+        <p>
 
-The EOS Token Sale will distributed daily over about 341 days. 1,000,000,000 (one billion) EOS tokens will be minted at the start of the sale. These tokens will be split into different rolling windows of availability. The tokens available in a window will be split proportional to all contributions made during the window period. 
+          The EOS Token Sale will distributed daily over about 341
+          days.  1,000,000,000 (one billion) EOS tokens will be minted
+          at the start of the sale.  These tokens will be split into
+          different rolling windows of availability.  The tokens
+          available in a window will be split proportional to all
+          contributions made during the window period.
 
-          </p>
+        </p>
 
-          For more details, please review the token sale <a
-          href=https://github.com/eosio/eos-token-sale>contract source
-          code</a>.
+        For more details, please review the token sale <a
+        href=https://github.com/eosio/eos-token-sale>contract source
+        code</a>.
 
+        ${web3.eth.accounts[0] ? `
           <div class=pane>
-            ${web3.eth.accounts[0] ? `
-              <table>
-                <tr>
-                  <th>Ethereum account</th>
-                  <td><code>${web3.eth.accounts[0]}</code></td>
-                </tr>
-                <tr>
-                  <th>EOS public key</th>
-                  <td style="text-align: left">
-                    <span style="color: gray">
-                      (no EOS public key registered)
-                    </span>
-                    <a href=# style="margin-left: 1rem">
-                      Register your EOS key
-                    </a>
-                  </td>
-                </tr>
-                <tr>
-                  <th>Token balances</th>
-                  <td style="text-align: left">0.0 ETH</td>
-                </tr>
-                <tr>
-                  <th></th>
-                  <td style="text-align: left">
-                    0.0 EOS
-                    <a href=# style="margin-left: 1rem; float: right">
-                      Transfer EOS tokens
-                    </a>
-                  </td>
-                </tr>
-                <tr>
-                  <th></th>
-                  <td style="text-align: left">
-                    0.0 EOS (unclaimed)
-                    <a href=# style="margin-left: 1rem; float: right">
-                      Claim EOS tokens
-                    </a>
-                  </td>
-                </tr>
-              </table>
-            ` : `
-              <div>
-                <h3>Ethereum account not found</h3>
-
-                It looks like an Ethereum client is available in your
-                browser, but I couldn&rsquo;t find any accounts.
-                If you&rsquo;re using MetaMask, you may need to unlock
-                your account. Please reload this page and try again.
-
-              </div>
-            `}
+            <table>
+              <tr>
+                <th>Ethereum account</th>
+                <td><code>${web3.eth.accounts[0]}</code></td>
+              </tr>
+              <tr>
+                <th>EOS public key</th>
+                <td style="text-align: left">
+                  <span style="color: gray">
+                    (no EOS public key registered)
+                  </span>
+                  <a href=# style="margin-left: 1rem">
+                    Register your EOS key
+                  </a>
+                </td>
+              </tr>
+              <tr>
+                <th>Token balances</th>
+                <td style="text-align: left">
+                  ${eth_balance.div(WAD)} ETH
+                  <a href=# id=buy-link
+                     style="margin-left: 1rem; float: right"
+                     onclick="show('buy-pane'), hide('buy-link')">
+                    Buy EOS tokens
+                  </a>
+                </td>
+              </tr>
+              <tr>
+                <th></th>
+                <td style="text-align: left">
+                  0.0 EOS
+                  <a href=# style="margin-left: 1rem; float: right">
+                    Transfer EOS tokens
+                  </a>
+                </td>
+              </tr>
+              <tr>
+                <th></th>
+                <td style="text-align: left">
+                  0.0 EOS (unclaimed)
+                  <a href=# style="margin-left: 1rem; float: right">
+                    Claim EOS tokens
+                  </a>
+                </td>
+              </tr>
+            </table>
           </div>
+          <div class="hidden pane" id=buy-pane>
+            <table>
+              <tr>
+                <th>Currently active sale window</th>
+                <td style="text-align: left">
+                  <span style="margin-right: .5rem">#2</span>
+                  (started 5 hours ago; ends in 18 hours)
+                </td>
+              </tr>
+              <tr>
+                <th>Tokens for sale in window</th>
+                <td style="text-align: left">
+                  ${formatWad(days[0].tokensForSale)} EOS
+                </td>
+              </tr>
+              <tr>
+                <th>Total contributions in window</th>
+                <td style="text-align: left">
+                  ${formatWad(days[0].contributions)} ETH
+                </td>
+              </tr>
+              <tr>
+                <th>Your contribution in window</th>
+                <td style="text-align: left">
+                  ${formatWad(days[0].contributions)} ETH
+                </td>
+              </tr>
+              <tr>
+                <th>Effective minimum price</th>
+                <td style="text-align: left">
+                  ${days[0].price.toFixed(9)} ETH/EOS
+                </td>
+              </tr>
+              <tr>
+                <th>Additional contribution</th>
+                <td style="text-align: left">
+                  <form>
+                    <input type=text required
+                           placeholder=${eth_balance.div(WAD)}>
+                    ETH
+                    <button style="margin-left: 1.5rem">
+                      Buy EOS tokens
+                    </button>
+                  </form>
+                </td>
+              </tr>
+            </table>
+          </div>
+        ` : `
+          <div class=pane>
+            <h3>Ethereum account not found</h3>
 
-          <table style="width: 100%; margin-bottom: 2rem">
+            It looks like an Ethereum client is available in your
+            browser, but I couldn&rsquo;t find any accounts.
+            If you&rsquo;re using MetaMask, you may need to unlock
+            your account. Please reload this page and try again.
+
+          </div>
+        `}
+
+        <table style="width: 100%; margin-bottom: 2rem">
+          <thead>
             <tr>
-              <th>Day</th>
+              <th>Window</th>
               <th>Tokens for sale</th>
               <th>Total contributions</th>
               <th>Your contribution</th>
               <th>Effective price</th>
             </tr>
+          </thead>
+          <tbody>
             ${days.map(day => {
               return `
                 <tr>
-                  <td>${day.name}</td>
+                  <td>#${day.name}</td>
                   <td>${formatWad(day.tokensForSale)} EOS</td>
                   <td>${formatWad(day.contributions)} ETH</td>
                   <td>${formatWad(day.contributions)} ETH</td>
@@ -121,12 +206,12 @@ The EOS Token Sale will distributed daily over about 341 days. 1,000,000,000 (on
                 </tr>
               `
             }).join("\n")}
-          </table>
-        `)
-      })
-    })
+          </tbody>
+        </table>
+      `)))
+    }))
   }
-}
+}, 500)
 
 function buy() {
   var wad = getValue("buy-input")
--- docs/lib.js
+++ docs/lib.js
@@ -1,19 +1,10 @@
-function repeat(x, n) {
-  return new Array(n + 1).join("x")
-}
-
-function iota(n) {
-  return repeat("x", n).split("").map((_, i) => i)
-}
-
-function render(id, html) {
-  document.getElementById(id).innerHTML = html
-}
-
-function getValue(id) {
-  return document.getElementById(id).value
-}
-
-function formatWad(wad) {
-  return String(wad).replace(/\..*/, "")
-}
+var byId       = id          => document.getElementById(id)
+var formatWad  = wad         => String(wad).replace(/\..*/, "")
+var getValue   = id          => byId(id).value
+var show       = id          => byId(id).classList.remove("hidden")
+var hide       = id          => byId(id).classList.add("hidden")
+var iota       = n           => repeat("x", n).split("").map((_, i) => i)
+var repeat     = (x, n)      => new Array(n + 1).join("x")
+var adjust     = (x, f)      => { x.innerHTML = f(x.innerHTML) }
+var append     = (id, html)  => { adjust(byId(id), x => x + html) }
+var render     = (id, html)  => { adjust(byId(id), () => html) }
