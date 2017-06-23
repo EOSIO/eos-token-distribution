e41a81af1172600e18aec6e341116c9f598401e4
Andrei Nadejde <andrei.nadejde@gmail.com>
2017-06-23 11:05:15 +0100

    Updated text on website

--- docs/index.js
+++ docs/index.js
@@ -159,7 +159,7 @@ var render = ({
 
     This will generate a private and public key pair for the EOS chain. The code is not mine.
     It is forked from the code the EOS team built. The difference is that this works in any browser without the web3 dependency.
-    It will work in any browser and you don't need any plugins.
+    It will work in any browser and you don't need any plugins. I've also removed everything else except the key generation bits.
 
   </p>
   Source code can be reviewed here: <a href="https://github.com/Nadejde/eos-token-sale">https://github.com/Nadejde/eos-token-sale</a>.
